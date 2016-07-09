90526939b00a69b7fdf17384a0e530a7850be39e
Aditya <addiittya2006@gmail.com>
2016-07-09 15:57:22 +0530

    model defn

--- fundsofhope/models.py
+++ fundsofhope/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class User(models.Model):
-    name = models.CharField(max_length=500, null=False)
-    email = models.EmailField(null=False)
-    phoneNo = models.CharField(max_length=15, null=False)
-    fbCred = models.CharField(max_length=100)
-    googleCred = models.CharField(max_length=100)
+    name = models.CharField(max_length=500)
+    email = models.EmailField()
+    phoneNo = models.CharField(max_length=15)
+    fbCred = models.CharField(max_length=100, blank=True)
+    googleCred = models.CharField(max_length=100, blank=True)
 
 
 class Ngo(models.Model):
-    name = models.CharField(max_length=500, null=False)
-    email = models.EmailField(null=False)
-    phoneNo = models.CharField(max_length=15, null=False)
+    name = models.CharField(max_length=500)
+    email = models.EmailField(blank=True)
+    phoneNo = models.CharField(max_length=15)
     ngoId = models.CharField(max_length=20, primary_key=True)
-    latitude = models.CharField(max_length=5)
-    longitude = models.CharField(max_length=5)
-    address = models.CharField(max_length=500, null=False)
+    latitude = models.CharField(max_length=5, blank=True)
+    longitude = models.CharField(max_length=5, blank=True)
+    address = models.CharField(max_length=500)
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=500, null=False)
-    description = models.CharField(max_length=10000, null=False)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=10000)
     startDate = models.DateTimeField()
     endDate = models.DateTimeField()
     cost = models.BigIntegerField()
-    status = models.BigIntegerField()
+    status = models.BigIntegerField(blank=True)
